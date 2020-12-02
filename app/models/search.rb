a4061db1d2b0985d19b183f440b27b3fc89bacef
Trev Chiu <gilbertgulliverchiu@yahoo.com.ph>
2020-12-01 21:31:04 -0400

    Added some classes

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,8 +6,8 @@ class Search < ApplicationRecord
       products = products.where(["category_id == ?", category_id]) if category_id.present?
       products = products.where(["brand_id == ?", brand_id]) if brand_id.present?
       products = products.where(["color_id == ?", color_id]) if color_id.present?
-      products = products.where(["min_price >= ?", min_price]) if min_price.present?
-      products = products.where(["max_price <= ?", max_price]) if max_price.present?
+      products = products.where(["price >= ?", min_price]) if min_price.present?
+      products = products.where(["price <= ?", max_price]) if max_price.present?
       products = products.where(["rating == ?", rating]) if rating.present?
     return products
   end
