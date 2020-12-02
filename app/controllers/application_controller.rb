6e22c8fe0a96a4930a9cbf0913c72824a92e1309
Trev Chiu <gilbertgulliverchiu@yahoo.com.ph>
2020-12-02 04:24:42 -0400

    Fixed Advance search query

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
 
-
-
   private
 
   def product_categories
