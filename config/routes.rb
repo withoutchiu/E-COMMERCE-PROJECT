a09280bd1db18e7f026d09b2305d59c08092c244
Trev Chiu <gilbertgulliverchiu@yahoo.com.ph>
2020-12-04 01:11:01 -0400

    New checkout associated routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
   resources :welcome
   resources :cart, only: %i[create destroy]
 
+  scope "/checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
